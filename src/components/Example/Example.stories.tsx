6859350f9381cc9b615479e64d096a50178d474b
Alona Krupenko <helengaraeva@gmail.com>
2023-07-25 13:46:55 +0300

    fix: test 1

--- src/components/Example/Example.stories.tsx
+++ src/components/Example/Example.stories.tsx
@@ -5,4 +5,5 @@ import { Example } from './Example';
 storiesOf('Example', module)
   .add('Default', () => <Example>Button</Example>)
   .add('Active', () => <Example active>Button</Example>)
-  .add('Disabled', () => <Example disabled>Button</Example>);
+  .add('Disabled', () => <Example disabled>Button</Example>)
+  .add('Disabled123', () => <Example disabled>12345</Example>);
