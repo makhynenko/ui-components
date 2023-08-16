61a7bf75e51c15dea7d0b455566f2da3f14ee499
AlonaKrupenko <88893788+AlonaKrupenko@users.noreply.github.com>
2023-08-16 11:36:16 +0300

    Modal (#4)
    * feat: created Modal
    
    * fix: added outside click
    
    * fix: improved modal outside click

---
+++ src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { storiesOf } from '@storybook/react';
+import { Modal } from './Modal';
+import { Button } from '../Button';
+
+storiesOf('Modal', module)
+  .add('Default', () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const openModal = () => {
+      setIsOpen(true)
+    }
+
+    const closeModal = () => {
+      setIsOpen(false)
+    }
+
+    return (
+      <>
+        <Button onClick={openModal}>{'Open modal'}</Button>
+        {isOpen
+          ? <Modal isOpen={isOpen} onClose={closeModal}>
+            <Button size='small' onClick={closeModal}>Close</Button>
+            <h2>What is Lorem Ipsum?</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </Modal>
+          : null}
+      </>
+    )
+  }
+  )
---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import classnames from 'classnames';
+import './modal.scss';
+
+export interface ModalProps {
+  className?: string,
+  children?: any,
+  isOpen: boolean,
+  onClose?: () => void,
+}
+
+export const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  children,
+  onClose,
+  className
+}) => {
+  const classes = classnames(
+    'Modal',
+    {
+      'Modal--isOpen': isOpen
+    },
+    className
+  );
+
+  const handleOutsideClick = (e) => {
+    onClose?.()
+  }
+
+  return (
+    <div className='ModalOverlay' onClick={handleOutsideClick} >
+      <div className={classes} onClick={(e) => { e.stopPropagation() }}> {children} </div >
+    </div>
+  );
+}
