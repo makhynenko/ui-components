06cb1f99e9dece242a34f5cc5f3d4acac217b0ef
AlonaKrupenko <88893788+AlonaKrupenko@users.noreply.github.com>
2023-08-21 13:25:46 +0300

    feat: created checkboxGroup component (#5)
    * feat: created checkboxGroup component
    
    * fix: refactored checkboxGroup component

---
+++ src/components/CheckboxGroup/CheckboxGroup.stories.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { storiesOf } from '@storybook/react';
+import { CheckboxGroup } from './CheckboxGroup';
+
+
+storiesOf('CheckboxGroup', module)
+  .add('Default', () => {
+
+    const optionsExample = [
+      { label: 'string', value: 'str', disabled: false },
+      { label: 'number', value: '123', disabled: false },
+      { label: 'boolean', value: 'boo', disabled: true }];
+
+    const selectedValuesExample = [];
+
+    const [selectedValues, setSelectedValues] = useState(selectedValuesExample)
+
+    const onSelect = (selectedValues) => {
+      setSelectedValues(selectedValues)
+    }
+
+    return (
+      <>
+        <CheckboxGroup options={optionsExample} selectedValues={selectedValues} onSelect={onSelect} size='small' />
+        <CheckboxGroup options={optionsExample} selectedValues={selectedValues} onSelect={onSelect} />
+        <CheckboxGroup options={optionsExample} selectedValues={selectedValues} onSelect={onSelect} size='large' />
+      </>
+    )
+  })
---
+++ src/types.ts
@@ -0,0 +1,2 @@
+export type Size = 'small' | 'medium' | 'large'
+export interface Option {label: string, value: string, disabled?: boolean}
