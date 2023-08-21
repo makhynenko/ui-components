f48428c13b3243d7d85353cae30875db0c24969b
Alona Krupenko <helengaraeva@gmail.com>
2023-08-21 13:31:35 +0300

    fix: resolved conflict

--- src/components/Example/Example.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { Example } from './Example';
-
-storiesOf('Example', module)
-  .add('Default', () => <Example>Button</Example>)
-  .add('Active', () => <Example active>Button</Example>)
-  .add('Disabled', () => <Example disabled>Button</Example>)
-  .add('Disabled123', () => <Example disabled>12345</Example>);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,6 @@
-// make sure you import all components into this file
-
-export * from './Example';
 export * from './Button';
 export * from './Input'
 export * from './Checkbox'
 export * from './Radio'
 export * from './Modal'
-
+export * from './CheckboxGroup'
