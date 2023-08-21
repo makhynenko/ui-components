f9e51286d5c8fd1ac3dfd9e62f8014c46d77fc64
Alona Krupenko <helengaraeva@gmail.com>
2023-08-21 21:18:47 +0300

    feat: created radioGroup component

---
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './radioGroup.scss';
+import { Option, Size } from '../../types';
+import { Radio } from '../Radio/Radio';
+import classnames from 'classnames';
+
+export interface RadioGroupProps {
+  size?: Size,
+  options: Option[],
+  selectedValue: string,
+  onSelect: (selectedValue: string) => void,
+  className?: string
+}
+
+export const RadioGroup: React.FC<RadioGroupProps> = ({
+  size, options, selectedValue, onSelect, className
+}) => {
+
+  const handleRadioChange = (elValue: string) => () => {
+    onSelect(elValue)
+  }
+
+  return (
+    <ul className={classnames('radioGroup', className)}>
+      {options.map((el) => (
+        <li className='radioItem' key={el.value}>
+          <Radio label={el.label} disabled={el.disabled} size={size} onChange={handleRadioChange(el.value)} value={selectedValue === el.value} />
+        </li>
+      ))}
+    </ul>
+  );
+}
