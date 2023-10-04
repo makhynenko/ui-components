73a5b4859ef107ebc57e8c3f40d5cb5b589dd8e1
di-kinosian <84967699+di-kinosian@users.noreply.github.com>
2023-10-04 17:06:26 +0300

    fix: change styles for radio and checkbox (#14)
    * fix: change styles for radio and checkbox components
    
    * docs: change yarn to npm
    
    * fix: delete extra styles
    
    ---------
    
    Co-authored-by: Diana Kinosian <d.kinosyan@avto.pro>

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -10,83 +10,117 @@ storiesOf('Checkbox', module).add('Default', () => {
     setValue(newValue);
   };
 
+  const inlineStyles: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '8px',
+  };
+
   return (
     <>
       <p>Default === Medium</p>
-      <Checkbox />
-      <Checkbox value={false} />
-      <Checkbox value={true} />
+      <div style={inlineStyles}>
+        <Checkbox />
+        <Checkbox value={false} />
+        <Checkbox value={true} />
+      </div>
       <br />
-      <Checkbox label='Lorem ipsum' onChange={onChange} value={true} />
-      <Checkbox label='Lorem ipsum' onChange={onChange} value={false} />
-      <Checkbox label='Lorem ipsum' onChange={onChange} value={value} />
+      <div style={inlineStyles}>
+        <Checkbox label='Lorem ipsum' onChange={onChange} value={true} />
+        <Checkbox label='Lorem ipsum' onChange={onChange} value={false} />
+        <Checkbox label='Lorem ipsum' onChange={onChange} value={value} />
+      </div>
       <br />
       <Checkbox label='Lorem ipsum' onChange={onChange} />
 
       <p>Small</p>
-      <Checkbox size='small' disabled value={true} label='Lorem ipsum 1111' />
+      <div style={inlineStyles}>
+        <Checkbox size='small' disabled value={true} label='Lorem ipsum 1111' />
 
-      <Checkbox size='small' disabled value={false} />
-      <Checkbox size='small' disabled value={true} />
+        <Checkbox size='small' disabled value={false} />
+        <Checkbox size='small' disabled value={true} />
+      </div>
       <br />
-      <Checkbox size='small' label='Lorem ipsum' onChange={onChange} disabled value={true} />
-      <Checkbox size='small' label='Lorem ipsum' onChange={onChange} disabled value={false} />
-      <Checkbox size='small' label='Lorem ipsum' onChange={onChange} disabled value={value} />
+      <div style={inlineStyles}>
+        <Checkbox size='small' label='Lorem ipsum' onChange={onChange} disabled value={true} />
+        <Checkbox size='small' label='Lorem ipsum' onChange={onChange} disabled value={false} />
+        <Checkbox size='small' label='Lorem ipsum' onChange={onChange} disabled value={value} />
+      </div>
       <br />
       <Checkbox size='small' label='Lorem ipsum' onChange={onChange} disabled />
 
       <br />
-      <Checkbox size='small' />
-      <Checkbox size='small' value={false} />
-      <Checkbox size='small' value={true} />
+      <div style={inlineStyles}>
+        <Checkbox size='small' />
+        <Checkbox size='small' value={false} />
+        <Checkbox size='small' value={true} />
+      </div>
       <br />
-      <Checkbox size='small' label='Lorem ipsum' onChange={onChange} value={true} />
-      <Checkbox size='small' label='Lorem ipsum' onChange={onChange} value={false} />
-      <Checkbox size='small' label='Lorem ipsum' onChange={onChange} value={value} />
+      <div style={inlineStyles}>
+        <Checkbox size='small' label='Lorem ipsum' onChange={onChange} value={true} />
+        <Checkbox size='small' label='Lorem ipsum' onChange={onChange} value={false} />
+        <Checkbox size='small' label='Lorem ipsum' onChange={onChange} value={value} />
+      </div>
       <br />
       <Checkbox size='small' label='Lorem ipsum' onChange={onChange} />
 
       <p>Medium</p>
-      <Checkbox size='medium' disabled />
-      <Checkbox size='medium' disabled value={false} />
-      <Checkbox size='medium' disabled value={true} />
+      <div style={inlineStyles}>
+        <Checkbox size='medium' disabled />
+        <Checkbox size='medium' disabled value={false} />
+        <Checkbox size='medium' disabled value={true} />
+      </div>
       <br />
-      <Checkbox size='medium' label='Lorem ipsum' onChange={onChange} disabled value={true} />
-      <Checkbox size='medium' label='Lorem ipsum' onChange={onChange} disabled value={false} />
-      <Checkbox size='medium' label='Lorem ipsum' onChange={onChange} disabled value={value} />
+      <div style={inlineStyles}>
+        <Checkbox size='medium' label='Lorem ipsum' onChange={onChange} disabled value={true} />
+        <Checkbox size='medium' label='Lorem ipsum' onChange={onChange} disabled value={false} />
+        <Checkbox size='medium' label='Lorem ipsum' onChange={onChange} disabled value={value} />
+      </div>
       <br />
       <Checkbox size='medium' label='Lorem ipsum' onChange={onChange} disabled />
 
       <br />
-      <Checkbox size='medium' />
-      <Checkbox size='medium' value={false} />
-      <Checkbox size='medium' value={true} />
+      <div style={inlineStyles}>
+        <Checkbox size='medium' />
+        <Checkbox size='medium' value={false} />
+        <Checkbox size='medium' value={true} />
+      </div>
       <br />
-      <Checkbox size='medium' label='Lorem ipsum' onChange={onChange} value={true} />
-      <Checkbox size='medium' label='Lorem ipsum' onChange={onChange} value={false} />
-      <Checkbox size='medium' label='Lorem ipsum' onChange={onChange} value={value} />
+      <div style={inlineStyles}>
+        <Checkbox size='medium' label='Lorem ipsum' onChange={onChange} value={true} />
+        <Checkbox size='medium' label='Lorem ipsum' onChange={onChange} value={false} />
+        <Checkbox size='medium' label='Lorem ipsum' onChange={onChange} value={value} />
+      </div>
       <br />
       <Checkbox size='medium' label='Lorem ipsum' onChange={onChange} />
 
       <p>Large</p>
-      <Checkbox size='large' disabled />
-      <Checkbox size='large' disabled value={false} />
-      <Checkbox size='large' disabled value={true} />
+      <div style={inlineStyles}>
+        <Checkbox size='large' disabled />
+        <Checkbox size='large' disabled value={false} />
+        <Checkbox size='large' disabled value={true} />
+      </div>
       <br />
-      <Checkbox size='large' label='Lorem ipsum' onChange={onChange} disabled value={true} />
-      <Checkbox size='large' label='Lorem ipsum' onChange={onChange} disabled value={false} />
-      <Checkbox size='large' label='Lorem ipsum' onChange={onChange} disabled value={value} />
+      <div style={inlineStyles}>
+        <Checkbox size='large' label='Lorem ipsum' onChange={onChange} disabled value={true} />
+        <Checkbox size='large' label='Lorem ipsum' onChange={onChange} disabled value={false} />
+        <Checkbox size='large' label='Lorem ipsum' onChange={onChange} disabled value={value} />
+      </div>
       <br />
       <Checkbox size='large' label='Lorem ipsum' onChange={onChange} disabled />
 
       <br />
-      <Checkbox size='large' />
-      <Checkbox size='large' value={false} />
-      <Checkbox size='large' value={true} />
+      <div style={inlineStyles}>
+        <Checkbox size='large' />
+        <Checkbox size='large' value={false} />
+        <Checkbox size='large' value={true} />
+      </div>
       <br />
-      <Checkbox size='large' label='Lorem ipsum' onChange={onChange} value={true} />
-      <Checkbox size='large' label='Lorem ipsum' onChange={onChange} value={false} />
-      <Checkbox size='large' label='Lorem ipsum' onChange={onChange} value={value} />
+      <div style={inlineStyles}>
+        <Checkbox size='large' label='Lorem ipsum' onChange={onChange} value={true} />
+        <Checkbox size='large' label='Lorem ipsum' onChange={onChange} value={false} />
+        <Checkbox size='large' label='Lorem ipsum' onChange={onChange} value={value} />
+      </div>
       <br />
       <Checkbox size='large' label='Lorem ipsum' onChange={onChange} />
     </>
--- src/components/Radio/Radio.stories.tsx
+++ src/components/Radio/Radio.stories.tsx
@@ -1,101 +1,132 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Radio } from './Radio'
+import { Radio } from './Radio';
 import { useState } from 'react';
 
-storiesOf('Radio', module)
-  .add('Default', () => {
-    const [value, setValue] = useState<boolean>(false);
+storiesOf('Radio', module).add('Default', () => {
+  const [value, setValue] = useState<boolean>(false);
 
-    const onChange = (newValue: boolean) => {
-      setValue(newValue)
-    }
+  const onChange = (newValue: boolean) => {
+    setValue(newValue);
+  };
 
-    return (
-      <>
-        <p>Default === Medium</p>
+  const inlineStyles: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '8px',
+  };
+
+  return (
+    <>
+      <p>Default === Medium</p>
+      <div style={inlineStyles}>
         <Radio onChange={onChange} value={value} />
         <Radio onChange={onChange} value={value} />
         <Radio onChange={onChange} value={value} />
-
-        <br />
-
+      </div>
+      <br />
+      <div style={inlineStyles}>
         <Radio value={false} />
         <Radio value={true} />
-        <br />
-        <Radio label="Lorem ipsum" onChange={onChange} value={true} />
-        <Radio label="Lorem ipsum" onChange={onChange} value={false} />
-        <Radio label="Lorem ipsum" onChange={onChange} value={value} />
-        <br />
-        <Radio label="Lorem ipsum" onChange={onChange} />
+      </div>
+      <br />
+      <div style={inlineStyles}>
+        <Radio label='Lorem ipsum' onChange={onChange} value={true} />
+        <Radio label='Lorem ipsum' onChange={onChange} value={false} />
+        <Radio label='Lorem ipsum' onChange={onChange} value={value} />
+      </div>
+      <br />
+      <Radio label='Lorem ipsum' onChange={onChange} />
 
-        <p>Small</p>
-        <Radio size="small" disabled />
-        <Radio size="small" disabled value={false} />
-        <Radio size="small" disabled value={true} />
-        <br />
-        <Radio size="small" label="Lorem ipsum" onChange={onChange} disabled value={true} />
-        <Radio size="small" label="Lorem ipsum" onChange={onChange} disabled value={false} />
-        <Radio size="small" label="Lorem ipsum" onChange={onChange} disabled value={value} />
-        <br />
-        <Radio size="small" label="Lorem ipsum" onChange={onChange} disabled />
+      <p>Small</p>
+      <div style={inlineStyles}>
+        <Radio size='small' disabled />
+        <Radio size='small' disabled value={false} />
+        <Radio size='small' disabled value={true} />
+      </div>
+      <br />
+      <div style={inlineStyles}>
+        <Radio size='small' label='Lorem ipsum' onChange={onChange} disabled value={true} />
+        <Radio size='small' label='Lorem ipsum' onChange={onChange} disabled value={false} />
+        <Radio size='small' label='Lorem ipsum' onChange={onChange} disabled value={value} />
+      </div>
+      <br />
+      <Radio size='small' label='Lorem ipsum' onChange={onChange} disabled />
 
-        <br />
-        <Radio size="small" />
-        <Radio size="small" value={false} />
-        <Radio size="small" value={true} />
-        <br />
-        <Radio size="small" label="Lorem ipsum" onChange={onChange} value={true} />
-        <Radio size="small" label="Lorem ipsum" onChange={onChange} value={false} />
-        <Radio size="small" label="Lorem ipsum" onChange={onChange} value={value} />
-        <br />
-        <Radio size="small" label="Lorem ipsum" onChange={onChange} />
+      <br />
+      <div style={inlineStyles}>
+        <Radio size='small' />
+        <Radio size='small' value={false} />
+        <Radio size='small' value={true} />
+      </div>
+      <br />
+      <div style={inlineStyles}>
+        <Radio size='small' label='Lorem ipsum' onChange={onChange} value={true} />
+        <Radio size='small' label='Lorem ipsum' onChange={onChange} value={false} />
+        <Radio size='small' label='Lorem ipsum' onChange={onChange} value={value} />
+      </div>
+      <br />
+      <Radio size='small' label='Lorem ipsum' onChange={onChange} />
 
-        <p>Medium</p>
-        <Radio size="medium" disabled />
-        <Radio size="medium" disabled value={false} />
-        <Radio size="medium" disabled value={true} />
-        <br />
-        <Radio size="medium" label="Lorem ipsum" onChange={onChange} disabled value={true} />
-        <Radio size="medium" label="Lorem ipsum" onChange={onChange} disabled value={false} />
-        <Radio size="medium" label="Lorem ipsum" onChange={onChange} disabled value={value} />
-        <br />
-        <Radio size="medium" label="Lorem ipsum" onChange={onChange} disabled />
+      <p>Medium</p>
+      <div style={inlineStyles}>
+        <Radio size='medium' disabled />
+        <Radio size='medium' disabled value={false} />
+        <Radio size='medium' disabled value={true} />
+      </div>
+      <br />
+      <div style={inlineStyles}>
+        <Radio size='medium' label='Lorem ipsum' onChange={onChange} disabled value={true} />
+        <Radio size='medium' label='Lorem ipsum' onChange={onChange} disabled value={false} />
+        <Radio size='medium' label='Lorem ipsum' onChange={onChange} disabled value={value} />
+      </div>
+      <br />
+      <Radio size='medium' label='Lorem ipsum' onChange={onChange} disabled />
 
-        <br />
-        <Radio size="medium" />
-        <Radio size="medium" value={false} />
-        <Radio size="medium" value={true} />
-        <br />
-        <Radio size="medium" label="Lorem ipsum" onChange={onChange} value={true} />
-        <Radio size="medium" label="Lorem ipsum" onChange={onChange} value={false} />
-        <Radio size="medium" label="Lorem ipsum" onChange={onChange} value={value} />
-        <br />
-        <Radio size="medium" label="Lorem ipsum" onChange={onChange} />
+      <br />
+      <div style={inlineStyles}>
+        <Radio size='medium' />
+        <Radio size='medium' value={false} />
+        <Radio size='medium' value={true} />
+      </div>
+      <br />
+      <div style={inlineStyles}>
+        <Radio size='medium' label='Lorem ipsum' onChange={onChange} value={true} />
+        <Radio size='medium' label='Lorem ipsum' onChange={onChange} value={false} />
+        <Radio size='medium' label='Lorem ipsum' onChange={onChange} value={value} />
+      </div>
+      <br />
+      <Radio size='medium' label='Lorem ipsum' onChange={onChange} />
 
+      <p>Large</p>
+      <div style={inlineStyles}>
+        <Radio size='large' disabled />
+        <Radio size='large' disabled value={false} />
+        <Radio size='large' disabled value={true} />
+      </div>
+      <br />
+      <div style={inlineStyles}>
+        <Radio size='large' label='Lorem ipsum' onChange={onChange} disabled value={true} />
+        <Radio size='large' label='Lorem ipsum' onChange={onChange} disabled value={false} />
+        <Radio size='large' label='Lorem ipsum' onChange={onChange} disabled value={value} />
+      </div>
+      <br />
+      <Radio size='large' label='Lorem ipsum' onChange={onChange} disabled />
 
-        <p>Large</p>
-        <Radio size="large" disabled />
-        <Radio size="large" disabled value={false} />
-        <Radio size="large" disabled value={true} />
-        <br />
-        <Radio size="large" label="Lorem ipsum" onChange={onChange} disabled value={true} />
-        <Radio size="large" label="Lorem ipsum" onChange={onChange} disabled value={false} />
-        <Radio size="large" label="Lorem ipsum" onChange={onChange} disabled value={value} />
-        <br />
-        <Radio size="large" label="Lorem ipsum" onChange={onChange} disabled />
-
-        <br />
-        <Radio size="large" />
-        <Radio size="large" value={false} />
-        <Radio size="large" value={true} />
-        <br />
-        <Radio size="large" label="Lorem ipsum" onChange={onChange} value={true} />
-        <Radio size="large" label="Lorem ipsum" onChange={onChange} value={false} />
-        <Radio size="large" label="Lorem ipsum" onChange={onChange} value={value} />
-        <br />
-        <Radio size="large" label="Lorem ipsum" onChange={onChange} />
-
-      </>
-    )
-  })
+      <br />
+      <div style={inlineStyles}>
+        <Radio size='large' />
+        <Radio size='large' value={false} />
+        <Radio size='large' value={true} />
+      </div>
+      <br />
+      <div style={inlineStyles}>
+        <Radio size='large' label='Lorem ipsum' onChange={onChange} value={true} />
+        <Radio size='large' label='Lorem ipsum' onChange={onChange} value={false} />
+        <Radio size='large' label='Lorem ipsum' onChange={onChange} value={value} />
+      </div>
+      <br />
+      <Radio size='large' label='Lorem ipsum' onChange={onChange} />
+    </>
+  );
+});
