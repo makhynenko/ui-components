03b37460721f823b59af8fb764849bede725da98
Diana Kinosian <d.kinosyan@avto.pro>
2023-10-06 12:56:29 +0300

    fix: secondary modify stories for tooltip

--- src/components/Tooltip/Tooltip.stories.tsx
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tooltip } from './Tooltip';
 import { storiesOf } from '@storybook/react';
-import { Button } from '../Button';
+import { RadioGroup } from '../RadioGroup';
 
 storiesOf('Tooltip', module).add('Default', () => {
+  const [newSelectedPosition, setNewSelectedPosition] = useState<
+    'top' | 'bottom' | 'right' | 'left'
+  >('top');
+  const [newSelectedAlign, setNewSelectedAlign] = useState<'start' | 'center' | 'end'>('center');
+  const [newSelectedWidth, setNewSelectedWidth] = useState<'100px' | '150px' | '300px'>('300px');
+
   const customStyle: React.CSSProperties = {
     backgroundColor: '#3E7BFA',
     borderRadius: '4px',
@@ -19,143 +25,77 @@ storiesOf('Tooltip', module).add('Default', () => {
     justifyContent: 'space-around',
     gap: '8px',
     alignItems: 'center',
+    height: '450px',
+  };
+
+  const PositionOptions = [
+    { label: 'top', value: 'top' },
+    { label: 'bottom', value: 'bottom' },
+    { label: 'right', value: 'right' },
+    { label: 'left', value: 'left' },
+  ];
+
+  const AlignOptions = [
+    { label: 'start', value: 'start' },
+    { label: 'center', value: 'center' },
+    { label: 'end', value: 'end' },
+  ];
+
+  const WidthOptions = [
+    { label: '100px', value: '100px' },
+    { label: '150px', value: '150px' },
+    { label: '300px', value: '300px' },
+  ];
+
+  const onPositionSelect = (newSelectedPosition) => {
+    setNewSelectedPosition(newSelectedPosition);
+  };
+
+  const onPositionAlign = (newSelectedAlign) => {
+    setNewSelectedAlign(newSelectedAlign);
+  };
+
+  const onPositionWidth = (newSelectedWidth) => {
+    setNewSelectedWidth(newSelectedWidth);
   };
 
   const randomText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mi nisl, mollis sit amet
-  iaculis non, dignissim nec sem. Etiam vulputate tristique tortor, id pharetra nisi
-  tempor ut.`;
+  iaculis non...`;
 
   return (
     <div>
-      <p>Position and default aligning (center)</p>
-      <div style={wrapperStyle}>
-        <Tooltip position='right' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'>
-          <div style={customStyle}>
-            placement=`right` <br />
-            align=`center`
-          </div>
-        </Tooltip>
-        <Tooltip position='top' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'>
-          <div style={customStyle}>
-            placement=`top`
-            <br /> align=`center`
-          </div>
-        </Tooltip>
-        <Tooltip position='bottom' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'>
-          <div style={customStyle}>
-            placement=`bottom`
-            <br />
-            align=`center`
-          </div>
-        </Tooltip>
-        <Tooltip position='left' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'>
-          <div style={customStyle}>
-            placement=`left` <br />
-            align=`center`
-          </div>
-        </Tooltip>
-      </div>
+      <p style={{ fontWeight: 'bold' }}>Choose needed position for further tooltip </p>
+      <RadioGroup
+        options={PositionOptions}
+        selectedValue={newSelectedPosition}
+        onSelect={onPositionSelect}
+        size='large'
+      />
 
-      <p>Position and align=start</p>
+      <p style={{ fontWeight: 'bold' }}>Choose needed align for further tooltip </p>
+      <RadioGroup
+        options={AlignOptions}
+        selectedValue={newSelectedAlign}
+        onSelect={onPositionAlign}
+        size='large'
+      />
+
+      <p style={{ fontWeight: 'bold' }}>Choose needed width for further tooltip </p>
+      <RadioGroup
+        options={WidthOptions}
+        selectedValue={newSelectedWidth}
+        onSelect={onPositionWidth}
+        size='large'
+      />
+
       <div style={wrapperStyle}>
         <Tooltip
-          position='right'
-          align='start'
-          text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+          position={newSelectedPosition}
+          align={newSelectedAlign}
+          text={randomText}
+          width={newSelectedWidth}
         >
-          <div style={customStyle}>
-            `placement=right` <br />
-            align=`start`
-          </div>
-        </Tooltip>
-        <Tooltip
-          position='top'
-          align='start'
-          text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-        >
-          <div style={customStyle}>
-            placement=`top` <br /> align=`start`
-          </div>
-        </Tooltip>
-        <Tooltip
-          position='bottom'
-          align='start'
-          text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-        >
-          <div style={customStyle}>
-            placement=`bottom` <br /> align=`start`
-          </div>
-        </Tooltip>
-        <Tooltip
-          position='left'
-          align='start'
-          text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-        >
-          <div style={customStyle}>
-            placement=`left` <br /> align=`start`
-          </div>
-        </Tooltip>
-      </div>
-
-      <p>Position and align=end</p>
-      <div style={wrapperStyle}>
-        <Tooltip
-          position='right'
-          align='end'
-          text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-        >
-          <div style={customStyle}>
-            placement=`right` <br /> align=`end`
-          </div>
-        </Tooltip>
-        <Tooltip
-          position='top'
-          align='end'
-          text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-        >
-          <div style={customStyle}>
-            placement=`top`
-            <br /> align=`end`
-          </div>
-        </Tooltip>
-        <Tooltip
-          position='bottom'
-          align='end'
-          text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-        >
-          <div style={customStyle}>
-            placement=`bottom`
-            <br /> align=`end`
-          </div>
-        </Tooltip>
-        <Tooltip
-          position='left'
-          align='end'
-          text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-        >
-          <div style={customStyle}>
-            placement=`left`
-            <br /> align=`end`
-          </div>
-        </Tooltip>
-      </div>
-
-      <p>Tooltip width</p>
-      <div style={wrapperStyle}>
-        <Tooltip position='top' text={randomText} width='100px'>
-          <Button>width=100px</Button>
-        </Tooltip>
-
-        <Tooltip position='top' text={randomText} width='150px'>
-          <Button>width=150px</Button>
-        </Tooltip>
-
-        <Tooltip position='top' text={randomText} width='300px'>
-          <Button>width=300px</Button>
-        </Tooltip>
-
-        <Tooltip position='top' text={randomText} width='500px'>
-          <Button>width=500px</Button>
+          <div style={customStyle}>Hover me</div>
         </Tooltip>
       </div>
     </div>
