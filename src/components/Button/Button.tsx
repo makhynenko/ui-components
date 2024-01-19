b1b9754ab289c67a061e0f78e6c5dce9aab89049
AlonaKrupenko <88893788+AlonaKrupenko@users.noreply.github.com>
2024-01-19 21:40:32 +0700

    F-3: Loading button (#34)
    * fix: added invalid prop for select
    
    * feat: created loading state for button
    
    * fix: styles fixing
    
    ---------
    
    Co-authored-by: Alona Krupenko <alonakrupenko@Alonas-MacBook-Pro.local>

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import classnames from 'classnames';
 import styles from './button.module.scss';
 import { ElementSize } from '../../types';
+import { Loader } from '../Loader';
 
 export interface ButtonProps {
   size?: ElementSize;
   variant?: 'primary' | 'secondary' | 'ghost';
   disabled?: boolean;
   width?: string;
+  isLoading?: boolean;
+  loadingText?: string;
 }
 
 export const Button: React.FC<ButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>> = ({
@@ -16,12 +19,14 @@ export const Button: React.FC<ButtonProps & React.ButtonHTMLAttributes<HTMLButto
   variant,
   className,
   width = 'fit-content',
+  isLoading,
+  loadingText = 'Loading',
   ...props
 }) => {
   const classes = classnames(
     styles.Button,
     {
-      [styles['Button--disabled']]: disabled,
+      [styles['Button--disabled']]: disabled || isLoading,
       [styles['Button--small']]: size === ElementSize.Small,
       [styles['Button--medium']]: size === ElementSize.Medium,
       [styles['Button--large']]: size === ElementSize.Large,
@@ -33,8 +38,20 @@ export const Button: React.FC<ButtonProps & React.ButtonHTMLAttributes<HTMLButto
   );
 
   return (
-    <button {...props} className={classes} disabled={disabled} style={{ width: width }}>
-      {props.children}
+    <button
+      {...props}
+      className={classes}
+      disabled={disabled || isLoading}
+      style={{ width: width }}
+    >
+      {isLoading ? (
+        <div className={styles.loadingWrapper}>
+          <Loader />
+          <p className={styles.loadingText}>{loadingText}</p>
+        </div>
+      ) : (
+        props.children
+      )}
     </button>
   );
 };
